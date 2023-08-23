0c7afa0306ea5165454af09ff758f5e3673d53ee
saidelhabhab <saidelhabhab@gmail.com>
2023-08-23 10:32:39 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import HelloVue from './components/HelloVue.vue';
+
+createApp({
+    components: {
+        HelloVue,
+    }
+}).mount('#app');
